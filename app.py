b577a064a2b760595410c322211b4ce8824fccc2
EASURAJA A <149556768+RajaSteveA@users.noreply.github.com>
2024-08-08 17:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ final_job_id=None
 
 connection = sql.connect(
         user="root",
-        host="localhost",
+        host="146.148.69.66",
         database="antony",
         password="root123",
         port="3306",
@@ -49,7 +49,7 @@ connection = sql.connect(
     )
 cursor = connection.cursor()
 create_tables_if_not_exist(cursor)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root123@localhost:3306/antony'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root123@146.148.69.66:3306/antony'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'tucEDtE44BbQLv7tXCivZkn1DbmKGsYb'
 
@@ -679,4 +679,4 @@ def default():
     return jsonify("hello world")
 
 if __name__ == '__main__':
-      app.run(debug=True,host="localhost",port=4000)
+      app.run(debug=True,host="0.0.0.0",port=8082)
